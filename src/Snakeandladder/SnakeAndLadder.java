178ba677c79329ee931af0cc65b4700fa1579e79
lkvreddy007 <lkvreddy007@gmail.com>
2020-09-24 13:33:12 +0530

    [Krishna]added-usecase2

--- src/Snakeandladder/SnakeAndLadder.java
+++ src/Snakeandladder/SnakeAndLadder.java
@@ -1,7 +1,10 @@
 package Snakeandladder;
 
 public class SnakeAndLadder {
+	static final int START_POSITION=0;
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder game");
+		int dice_value=1+(int) Math.floor(Math.random()*10)%6;
+		//System.out.println(dice_value);
 	}
 }
